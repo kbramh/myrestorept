c3ae6ef91d11482b86b9fe17e20cfba0beb1b03a
kbramh <kylebramhall2@hotmail.com>
2026-07-11 00:36:42 -0700

    feat: added css styling to search input field and button

--- js/search-bar.js
+++ js/search-bar.js
@@ -1,9 +1,8 @@
 const searchBarPanel = `
-        <div>
-        <input input type="text" id="search" placeholder="Search..." /><button>
-          Go!
-        </button>
-      </div>
+        <div class="search-bar-form">
+         <input class="search-bar-input" type="text" id="search" placeholder="Search..." />
+         <button type="button" class="search-bar-button">Go!</button>
+        </div>
     `;
 
 const searchBarElement = document.querySelector('searchBar');
